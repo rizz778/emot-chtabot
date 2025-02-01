9076c031c0cee4af313761fd4fd4823b9f3d51b2
rizz778 <arorasuvir52@gmail.com>
2025-02-02 01:00:30 +0530

    token balance logic added

--- server/models/User.js
+++ server/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
+  tokens:{type:Number, default:10},
 });
 
 // Hash password before saving to DB
