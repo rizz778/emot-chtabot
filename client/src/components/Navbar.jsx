93c90bc1aa9340a601f146146034025397831d3b
VANSHKAKKAR04 <125470711+VANSHKAKKAR04@users.noreply.github.com>
2025-02-01 21:55:07 +0530

    updated model with chat history and small change to font colour

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,31 +1,31 @@
 import { NavLink } from "react-router-dom";
-import logo6 from '../assets/images/logo6.jpg';
+import logo6 from "../assets/images/logo6.jpg";
 
 const Navbar = () => {
   return (
-    <header className='flex items-center justify-between p-4 bg-gradient-to-r from-pink-300 via-[#f64a8a] to-purple-400 h-20'>
+    <header className="flex items-center justify-between p-4 bg-gradient-to-r from-pink-300 via-[#f64a8a] to-purple-400 h-20">
       {/* Navigation Links (Left) */}
-      <nav className='flex gap-8 font-medium text-lg'>
+      <nav className="flex gap-8 font-medium text-lg">
         <NavLink
-          to='/about'
+          to="/about"
           className={({ isActive }) =>
-            isActive ? "text-white font-bold" : "text-white hover:text-blue-200"
+            isActive ? "text-black font-bold" : "text-black hover:text-blue-200"
           }
         >
           About
         </NavLink>
         <NavLink
-          to='/login'
+          to="/login"
           className={({ isActive }) =>
-            isActive ? "text-white font-bold" : "text-white hover:text-blue-200"
+            isActive ? "text-black font-bold" : "text-black hover:text-blue-200"
           }
         >
           Login
         </NavLink>
         <NavLink
-          to='/signup'
+          to="/signup"
           className={({ isActive }) =>
-            isActive ? "text-white font-bold" : "text-white hover:text-blue-200"
+            isActive ? "text-black font-bold" : "text-black hover:text-blue-200"
           }
         >
           Signup
@@ -33,9 +33,9 @@ const Navbar = () => {
       </nav>
 
       {/* Logo (Center) */}
-      <div className='absolute left-1/2 transform -translate-x-1/2'>
-        <NavLink to='/'>
-          <img src={logo6} alt='logo' className='w-28 h-18 object-contain' />
+      <div className="absolute left-1/2 transform -translate-x-1/2">
+        <NavLink to="/">
+          <img src={logo6} alt="logo" className="w-28 h-18 object-contain" />
         </NavLink>
       </div>
     </header>
