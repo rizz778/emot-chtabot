a3eb779c1ad34bad202923edc2ecce7b23df85ea
twinkle831 <dtwinkle831@gmail.com>
2025-02-07 15:54:01 +0530

    deployment error log

--- client/src/hooks/useChat.jsx
+++ client/src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const backendUrl = import.meta.env.VITE_API_URL || "https://emot-chatbot.onrender.com";
 
 const ChatContext = createContext();
 
