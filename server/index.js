f1086de2675a126d364d39a67f9bb9096b2e250a
rizz778 <arorasuvir52@gmail.com>
2025-03-20 02:08:05 +0530

    fix6

--- server/index.js
+++ server/index.js
@@ -26,12 +26,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://emot-chtabot-2.onrender.com", // Allow your frontend origin
-    credentials: true, // Allow cookies if needed
-  })
-);
+
 // Session middleware for Passport
 app.use(
   session({
