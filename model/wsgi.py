d68996243aa961d49cbf839629a81ea738cc7bdb
rizz778 <arorasuvir52@gmail.com>
2025-04-04 01:28:57 +0530

    deploy fix 10

--- model/wsgi.py
+++ model/wsgi.py
@@ -1,4 +1,6 @@
+import os
 from gem_rag_new import app
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000
+    app.run(host="0.0.0.0", port=port)
