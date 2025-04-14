643276411c10f1a4e1a3a4ba9d6abb8a14940f9a
rizz778 <arorasuvir52@gmail.com>
2025-04-14 14:14:47 +0530

    fixxy

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
       if (isAuthenticated) {
         try {
           const token = localStorage.getItem("token");
-          const response = await axios.get('http://https://emot-chtabot.onrender-1.com', {
+          const response = await axios.get('https://emot-chtabot.onrender-1.com/api/profile', {
             headers: { Authorization: `Bearer ${token}` }
           });
           console.log("User profile response:", response.data.userProfile);
